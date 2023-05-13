610292f9226304c559a98bbc636a68cba894a2f7
pybnen <ha.reisinger@gmail.com>
2023-05-13 16:22:45 +0200

    Format files

--- VarManager.h
+++ VarManager.h
@@ -14,60 +14,62 @@
 class VarManager
 {
 private:
-  std::map<Var,Var> prop_to_original;
-  std::map<Var, Var> prop_to_ferp;
-  std::map<Var, std::vector<Lit>*> prop_to_annotation;
-  std::vector<std::vector<Lit>*> annotations;
-  std::vector<bool> occurring_prop;
-  std::vector<uint32_t> clause_origin;
+    std::map<Var, Var> prop_to_original;
+    std::map<Var, Var> prop_to_ferp;
+    std::map<Var, std::vector<Lit> *> prop_to_annotation;
+    std::vector<std::vector<Lit> *> annotations;
+    std::vector<bool> occurring_prop;
+    std::vector<uint32_t> clause_origin;
+
 public:
-  inline VarManager();
-  ~VarManager();
-  
-  int addVariables(const std::vector<Var>& prop, const std::vector<Var>& orig, const std::vector<Lit>& anno);
-  void computeNames();
-  void writeExpansions(FILE* file);
-  inline void pushClauseOrigin(uint32_t cl);
-  inline void addOccurence(Var v);
-  inline uint32_t numClauseOrigins() const;
-  inline uint32_t getClauseOrigin(uint32_t clause_id) const;
-  inline Lit getLitFerp(const Lit l) const;
+    inline VarManager();
+    ~VarManager();
+
+    int addVariables(const std::vector<Var> &prop, const std::vector<Var> &orig, const std::vector<Lit> &anno);
+    void computeNames();
+    void writeExpansions(FILE *file);
+    inline void pushClauseOrigin(uint32_t cl);
+    inline void addOccurence(Var v);
+    inline uint32_t numClauseOrigins() const;
+    inline uint32_t getClauseOrigin(uint32_t clause_id) const;
+    inline Lit getLitFerp(const Lit l) const;
 };
 
 //////////// INLINE IMPLEMENTATIONS ////////////
 
 VarManager::VarManager()
 {
-  clause_origin.push_back(0);
+    clause_origin.push_back(0);
 }
 
 void VarManager::pushClauseOrigin(uint32_t cl)
 {
-  clause_origin.push_back(cl);
+    clause_origin.push_back(cl);
 }
 
 void VarManager::addOccurence(Var v)
 {
-  if (v >= occurring_prop.size())
-    occurring_prop.resize(v + 1, false);
-  occurring_prop[v] = true;
+    if (v >= occurring_prop.size())
+        occurring_prop.resize(v + 1, false);
+    occurring_prop[v] = true;
 }
 
 uint32_t VarManager::numClauseOrigins() const
 {
-  return (uint32_t)clause_origin.size() - 1;
+    return (uint32_t)clause_origin.size() - 1;
 }
 
 uint32_t VarManager::getClauseOrigin(uint32_t clause_id) const
 {
-  return clause_origin[clause_id];
+    return clause_origin[clause_id];
 }
 
 Lit VarManager::getLitFerp(const Lit l) const
 {
-  auto it = prop_to_ferp.find(var(l));
-  if(it == prop_to_ferp.end()) return 0;
-  return make_lit(it->second, sign(l));
+    auto it = prop_to_ferp.find(var(l));
+    if (it == prop_to_ferp.end())
+        return 0;
+    return make_lit(it->second, sign(l));
 }
 
-#endif //TOFERP_VARMENAGER_H
+#endif // TOFERP_VARMENAGER_H
--- common.h
+++ common.h
@@ -7,47 +7,46 @@
 
 #include <stdlib.h>
 
-typedef int             Lit;
-typedef int*            lit_iterator;
-typedef const int*      const_lit_iterator;
+typedef int Lit;
+typedef int *lit_iterator;
+typedef const int *const_lit_iterator;
 
-typedef unsigned        Var;
-typedef unsigned*       var_iterator;
-typedef const unsigned* const_var_iterator;
+typedef unsigned Var;
+typedef unsigned *var_iterator;
+typedef const unsigned *const_var_iterator;
 
 /// Returns variable corresponding to literal \a l
 inline Var var(Lit l)
 {
-  return std::abs(l);
+	return std::abs(l);
 }
 
 /// Returns true if literal \a l is negated
 inline bool sign(Lit l)
 {
-  return l < 0;
+	return l < 0;
 }
 
 /// Constructs literal from variable \a v and \a sign
 inline Lit make_lit(Var v, bool sign)
 {
-  return (Lit)((v ^ -((Lit)sign)) + (Lit)sign);
+	return (Lit)((v ^ -((Lit)sign)) + (Lit)sign);
 }
 
 /// Defines a custom order relation for literals
 inline bool lit_order(Lit a, Lit b)
 {
-  const Var va = var(a);
-  const Var vb = var(b);
-  return va < vb || (va == vb && a < b);
+	const Var va = var(a);
+	const Var vb = var(b);
+	return va < vb || (va == vb && a < b);
 }
 
 /// Enum representing quantifier types in QBF
 enum QuantType
 {
-  NONE,
-  EXISTS,
-  FORALL
+	NONE,
+	EXISTS,
+	FORALL
 };
 
-
-#endif //NANOQBF_COMMON_H
+#endif // NANOQBF_COMMON_H
