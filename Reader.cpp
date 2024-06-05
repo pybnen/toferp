e1208bf82889613dedd88f42a33f3be0db7ea078
Harald Reisinger <ha.reisinger@gmail.com>
2024-06-05 15:53:02 +0200

    For false qbf only variable mapping is needed

--- Reader.cpp
+++ Reader.cpp
@@ -134,6 +134,10 @@ int AnnotationReader::readCNF(VarManager &mngr)
         }
     }
 
+    if (!mngr.is_sat) {
+        return 0;
+    }
+
     if (*stream == 'p')
     {
         skipLine(stream);
